3a3a0d3709a98dc74cf8bf5f76bb636951f0a2ca
rdbahr <48165244+rdbahr@users.noreply.github.com>
2025-08-18 12:18:58 -0600

    Update pybaseball_to_es.py

--- pybaseball_to_es.py
+++ pybaseball_to_es.py
@@ -1,6 +1,9 @@
 from pybaseball import schedule_and_record
 import datetime as dt
 import pandas as pd
+import sys
+import re
+import os
 from elasticsearch import Elasticsearch, helpers
 import configparser
 
@@ -11,26 +14,53 @@ es = Elasticsearch(
     cloud_id=config['ELASTIC']['cloud_id'],
     basic_auth=(config['ELASTIC']['user'], config['ELASTIC']['password'])
 )
-#es.info()
 
-year = '2024'
-team = 'ARI'
+ALE = pd.DataFrame({ 'Team': ['BAL', 'BOS', 'NYY', 'TBR', 'TOR'], 'League': 'AL', 'Division': 'ALE' })
+ALC = pd.DataFrame({ 'Team': ['CLE', 'CHW', 'DET', 'KCR', 'MIN'], 'League': 'AL', 'Division': 'ALC' })
+ALW = pd.DataFrame({ 'Team': ['OAK', 'HOU', 'LAA', 'SEA', 'TEX'], 'League': 'AL', 'Division': 'ALW' })
+NLE = pd.DataFrame({ 'Team': ['ATL', 'MIA', 'NYM', 'PHI', 'WSN'], 'League': 'NL', 'Division': 'NLE' })
+NLC = pd.DataFrame({ 'Team': ['CHC', 'CIN', 'MIL', 'PIT', 'STL'], 'League': 'NL', 'Division': 'NLC' })
+NLW = pd.DataFrame({ 'Team': ['ARI', 'COL', 'LAD', 'SDP', 'SFG'], 'League': 'NL', 'Division': 'NLW' })
 
-team_rec = schedule_and_record(int(year), team)
+ALL = pd.concat([ALE, ALC, ALW, NLE, NLC, NLW], ignore_index=True)
+ALLOpp = ALL.rename(columns={'Team': 'Opponent', 'League': 'OpponentLeague', 'Division': 'OpponentDivision'})
 
-team_rec['Date'] = team_rec['Date'].astype(str) + ' ' + year
-team_rec['Date'] = team_rec['Date'].str.replace(r'\(\d\)\s', '', regex=True)
-team_rec['Date'] = pd.to_datetime(team_rec['Date'], format='%A, %b %d %Y').dt.strftime('%Y-%m-%dT%H:%M:%SZ')
+if re.match("\d{4}", sys.argv[1]):
+  year = sys.argv[1]
+else:
+  year = '2024'
+  print("Year not passed or invalid, using " + year + '.')
 
-# Write to csv
-#f_name = team + '_rec_' + year + '.csv'
-#team_rec.to_csv(f_name, index=False)
+if sys.argv[2] == 'ALL':
+  team_list = ALL
+elif int(ALL['Team'].str.contains(sys.argv[2]).value_counts().get(True)) == 1:
+  team_list = ALL[ALL['Team'].str.contains(sys.argv[2])]
 
-# TO DO: Convert DataFrame to JSON docs
+rename_cols = {
+  'Tm': 'Team', 'Opp': 'Opponent', 'W/L': 'WinLoss', 'R': 'Runs', 'RA': 'RunsAllowed',
+  'Inn': 'Innings', 'GB': 'GamesBack', 'Win': 'PitcherWin', 'Loss': 'PitcherLoss',
+  'Save': 'PitcherSave', 'Time': 'Duration', 'D/N': 'DayNight', 'cLI': 'ChampionshipLeverageIndex'
+}
 
-for hit in team_rec:
+for index, row in team_list.iterrows():
+  team = row['Team']
+  print(team)
+  team_rec = schedule_and_record(int(year), team)
+  team_rec.rename(columns=rename_cols, inplace=True)
+  # add league and division columns to team_rec
+  team_rec = pd.merge(team_rec, ALL, on='Team', how='left')
+  team_rec = pd.merge(team_rec, ALLOpp, on='Opponent', how='left')
+  team_rec['Date'] = team_rec['Date'].astype(str) + ' ' + year
+  team_rec['Date'] = team_rec['Date'].str.replace(r'\(\d\)\s', '', regex=True)
+  team_rec['Date'] = pd.to_datetime(team_rec['Date'], format='%A, %b %d %Y').dt.strftime('%Y-%m-%dT%H:%M:%SZ')
+  team_rec['GamesBack'] = team_rec['GamesBack'].str.replace('Tied', '0.0')
+  team_rec['GamesBack'] = team_rec['GamesBack'].str.replace(r'up\s?', '-', regex=True)
+  team_rec['ChampionshipLeverageIndex'] = team_rec['ChampionshipLeverageIndex'].str.replace(r'(-+|\++)', '', regex=True)
+  f_name = team + '_rec_' + year + '.csv'
+  team_rec.to_csv(os.path.join('results', f_name), index=False)
+  for index_rec, row_rec in team_rec.iterrows():
+    row_rec_json = row_rec.to_json()
     es.index(
-       index='mlb_games',
-        document=hit
+      index='mlb_games',
+      document=row_rec_json
     )
-
